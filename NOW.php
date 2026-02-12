22c6c8b6c2dcd20c2e49c5b3f8b1721f7e875487
Daudidesigner <daudimtale159@gmail.com>
2026-02-12 20:35:22 +0300

    Add files via upload

---
+++ NOW.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MR ROBOT</title> 
+  <link rel="icon" type="image/x-icon" href="mr_robot.png">
+</head>
+<body>
+  <?php
+  echo 'my first php page';
+  ?>
+</body>
+</html>
